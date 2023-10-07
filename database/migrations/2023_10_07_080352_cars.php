ebf0a5100634e1f84d70947197dd211769e87158
Ed Khalturin <e.khalturin@qsoft.ru>
2023-10-07 11:06:44 +0300

    init

---
+++ database/migrations/2023_10_07_080352_cars.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('brand', 255);
+            $table->string('model', 255);
+            $table->string('color', 255);
+            $table->string('plate', 50)->unique();
+            $table->boolean('is_parked');
+            $table->unsignedBigInteger('owner_id');
+
+            $table->foreign('owner_id')->references('id')->on('clients')
+                ->cascadeOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('cars');
+    }
+};
