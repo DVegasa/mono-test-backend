ddce4b41be3b3a53fb53be67d3c084c071ca09d0
Ed Khalturin <e.khalturin@qsoft.ru>
2023-10-07 11:32:26 +0300

    Базовые маршруты и контроллеры

---
+++ app/Http/Controllers/CarsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class CarsController extends Controller
+{
+
+    public function get(Request $request): Response
+    {
+        return response(['status' => 'wip']);
+    }
+
+    public function getList(Request $request): Response
+    {
+        return response(['status' => 'wip']);
+    }
+
+    public function create(Request $request): Response
+    {
+        return response(['status' => 'wip']);
+    }
+
+    public function update(Request $request): Response
+    {
+        return response(['status' => 'wip']);
+    }
+
+    public function delete(Request $request): Response
+    {
+        return response(['status' => 'wip']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,17 +1,26 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\CarsController;
+use App\Http\Controllers\ClientsController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
+Route::prefix('/v1')->group(function () {
+    Route::get('/ping', fn() => response(['ping' => 'pong']));
 
+    Route::prefix('/cars')->group(function () {
+        Route::get('/get', [CarsController::class, 'get']);
+        Route::get('/getList', [CarsController::class, 'getList']);
+        Route::post('/create', [CarsController::class, 'create']);
+        Route::post('/update', [CarsController::class, 'update']);
+        Route::post('/delete', [CarsController::class, 'delete']);
+    });
+
+    Route::prefix('/clients')->group(function () {
+        Route::get('/get', [ClientsController::class, 'get']);
+        Route::get('/getList', [ClientsController::class, 'getList']);
+        Route::post('/create', [ClientsController::class, 'create']);
+        Route::post('/update', [ClientsController::class, 'update']);
+        Route::post('/delete', [ClientsController::class, 'delete']);
+    });
+});
